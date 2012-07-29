0858386d185b57f11c57b57750d94474f68a6092
Biswarup Chakravarty <bchakravarty@tavisca.com>
2012-07-29 23:21:50 +0530

    added files

---
+++ js/String.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    String.format = function (text) {
+        if (arguments.length <= 1) {
+            return text;
+        }
+        var tokenCount = arguments.length - 2;
+        for (var token = 0; token <= tokenCount; token++) {
+            //iterate through the tokens and replace their placeholders from the original text in order
+            text = text.replace(new RegExp("\\{" + token + "\\}", "gi"),
+                                                arguments[token + 1]);
+        }
+        return text;
+    };
+
+    String.toSearchString = function (text) {
+        if (typeof (text) == 'undefined')
+            text = '';
+
+        var result = '';
+        for (var x = 0; x < text.length; x = x + 1) {
+            if (' .,;_#'.indexOf(text[x]) == -1)
+                result += text[x];
+        }
+
+        result = result.toLowerCase();
+
+        return result;
+    }
+
+    String.contains = function (s1, s2) {
+        return (s1.indexOf(s2) != -1);
+    }
+
+    String.startsWith = function (s1, s2) {
+        return (s1.indexOf(s2) == 0);
+    }
+
+    window.dateFromWcf = function(input, throwOnInvalidInput) {
+        var pattern = /Date\(([^)]+)\)/;
+        var results = pattern.exec(input);
+        if (results.length != 2) {
+            if (!throwOnInvalidInput) {
+                return s;
+            }
+            throw new Error(s + " is not .net json date.");
+        }
+        return new Date(parseFloat(results[1]));
+    }
+} ());
---
+++ js/auth.js
@@ -0,0 +1,29 @@
+if (!Gossamer) window.Gossamer = {};
+
+Gossamer.authentication = new (function() {
+	
+	var sessionId = null;
+	
+	this.getSessionId = function() {
+		return sessionId || '';
+	}
+	
+	this.setApiKey = function(apikey) {
+		apikey = apikey || 'kHfGM9t2OdT1i7EEuQsLqO0Po16KMowftXqCnYlLY54=';
+		var request = {
+			ApiKey: apikey,
+			IsNonSliding: false,
+			UsageCount: -1,
+			WindowTime: 60
+		};
+		var url = Gossamer.storage.urlFactory.session.getCreateSessionUrl();
+		Gossamer.utils.ajax.put(url, request, true, function(data) {
+			if (data.Session && data.Session.SessionKey) sessionId = data.Session.SessionKey;
+		}, function() {
+			throw new Error("Couldn't get session key from server.");
+		});
+	}
+	
+})();
+
+Gossamer.init = Gossamer.authentication.setApiKey;
